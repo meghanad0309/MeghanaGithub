069346017816ce15e65495697a378e9dcce37b25
Hp <Hp@DESKTOP-FIF2BTC>
2024-08-13 11:02:33 +0530

    Added leads and calendar

---
+++ MeghanaGithub/src/test/java/callendar/ToCreateCalendar.java
@@ -0,0 +1,10 @@
+package callendar;
+
+public class ToCreateCalendar {
+
+	public static void main(String[] args) {
+	System.out.println("Calendar created successfully");
+
+	}
+
+}
---
+++ MeghanaGithub/src/test/java/leads/ToCreateLead.java
@@ -0,0 +1,10 @@
+package leads;
+
+public class ToCreateLead {
+
+	public static void main(String[] args) {
+	System.out.println("Lead created successfully");
+
+	}
+
+}
